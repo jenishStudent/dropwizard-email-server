66f41c457bc049fdb220538e413195957459eb6f
jenishkanani <jkanani1@termail.umd.edu>
2021-04-07 23:37:00 -0400

    test

---
+++ src/main/java/no/avexis/dropwizard/mail/SSLMailerClient.java
@@ -0,0 +1,11 @@
+package no.avexis.dropwizard.mail;
+
+import javax.mail.Session;
+
+public class SSLMailerClient {
+
+    SSLMailerClient(Session session, String fromName, String fromAddress) {
+
+    }
+
+}
